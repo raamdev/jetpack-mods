8174126391223e8d981e0636d61f23b717b5d8d0
Raam Dev <raam@raamdev.com>
2014-04-08 18:48:06 -0400

    Increase max message size to 116 chars; add quote format support

--- modules/publicize.php
+++ modules/publicize.php
@@ -4,13 +4,13 @@
 function jetpack_enhancements_publicize_custom_message( $post_ID ) {
 	$post = get_post( $post_ID );
 
-	/** If the post being publicized is an aside, let's use the entire aside as
+	/** If the post being publicized is an aside or a quote, let's use the entire content as
 	    the Custom Message instead of just the title, if it will fit on Twitter. */
-	if ( 'aside' == get_post_format( $post->ID ) ) {
+	if ( 'aside' == get_post_format( $post->ID ) || 'quote' == get_post_format( $post->ID ) ) {
 		$custom_message = apply_filters('the_content', $post->post_content);
 		$custom_message = strip_tags( $custom_message );
 		$custom_message = trim( $custom_message );
-		if ( strlen( $custom_message ) <= 100 ) { // This should be 116 but Publicize acts weird
+		if ( strlen( $custom_message ) <= 116 ) {
 			update_post_meta( $post->ID, '_wpas_mess', $custom_message );
 		}
 	}
