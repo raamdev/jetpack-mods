861a7f55b309d707f67b26ba02df6f0de9e341e1
Raam Dev <raam@raamdev.com>
2014-10-16 23:44:44 -0400

    Update version number; reformat code

--- jetpack-mods.php
+++ jetpack-mods.php
@@ -2,19 +2,19 @@
 /*
 Plugin Name: Jetpack Mods
 
-Version: 140323
+Version: 141016
 Stable tag: 140323
-Requires at least: 2.9.2
-Tested up to: 3.8
+Requires at least: 3.7
+Tested up to: 4.0
 
 Plugin URI: http://wordpress.org/extend/plugins/jetpack-mods
-Description: Provides various enhacements to the Jetpack plugin, including simplified sharing icons and tweaks to Publicize.
+Description: Provides various enhancements to the Jetpack plugin, including simplified sharing icons and tweaks to Publicize.
 
 Text Domain: jetpack-mods
 Domain Path: /langs
 
 Author: Raam Dev
-Author URI: http://raamdev.com/
+Author URI: https://raam.org/
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=82JLPADVDAB2Q
 Contributors: raamdev
 
@@ -22,20 +22,16 @@ License: GNU General Public License v3 or later.
 
 */
 
-if(!defined('WPINC')) // MUST have WordPress.
-	exit('Do NOT access this file directly: '.basename(__FILE__));
+if ( ! defined( 'WPINC' ) ) // MUST have WordPress.
+{
+	exit( 'Do NOT access this file directly: ' . basename( __FILE__ ) );
+}
 
 
-/** Get a list of active JetPack modules so that we only load enhancements for those modules */
-$jetpack_active_modules = get_option('jetpack_active_modules');
+/* Get a list of active JetPack modules so that we only load enhancements for those modules */
+$jetpack_active_modules = get_option( 'jetpack_active_modules' );
 
-/**
- * Enhancements to the JetPack Publicize Module
- *
- * - If the post being publicized is an Aside post format
- *   the entire post content will be used as the custom message
- *   if the post content is 100 characters or less.
- */
+/* Enhancements to the JetPack Publicize Module */
 if ( $jetpack_active_modules && in_array( 'publicize', $jetpack_active_modules ) ) {
-	require_once('modules/publicize.php');
+	require_once( 'modules/publicize.php' );
 }
--- modules/publicize.php
+++ modules/publicize.php
@@ -5,9 +5,9 @@ function jetpack_mods_publicize_custom_message( $post_ID ) {
 	$post = get_post( $post_ID );
 
 	/** If the post being publicized is an Aside, let's use the entire content as
-	    the Custom Message instead of just the title, if it will fit on Twitter. */
+	 * the Custom Message instead of just the title, if it will fit on Twitter. */
 	if ( 'aside' === get_post_format( $post->ID ) ) {
-		$custom_message = apply_filters('the_content', $post->post_content);
+		$custom_message = apply_filters( 'the_content', $post->post_content );
 		$custom_message = strip_tags( $custom_message );
 		$custom_message = trim( $custom_message );
 
@@ -17,25 +17,25 @@ function jetpack_mods_publicize_custom_message( $post_ID ) {
 	}
 
 	/** If the post being publicized is a Quote, let's use the entire content as
-	the Custom Message instead of just the title, if it will fit on Twitter. */
+	 * the Custom Message instead of just the title, if it will fit on Twitter. */
 	if ( 'quote' === get_post_format( $post->ID ) ) {
-		$custom_message = apply_filters('the_content', $post->post_content);
+		$custom_message = apply_filters( 'the_content', $post->post_content );
 
 		/* If our quote has <cite> tags AND it doesn't contain any double-quotes THEN
 		   let's wrap the quote itself in double-quotes for better presentation on Twitter. */
-		if (preg_match('/<cite>/', $custom_message) && strpos(strip_tags($custom_message), '"') === FALSE) {
+		if ( preg_match( '/<cite>/', $custom_message ) && strpos( strip_tags( $custom_message ), '"' ) === false ) {
 
-			$cite_start_pos = stripos($custom_message, '<cite>');
-			$quote = substr($custom_message, 0, $cite_start_pos); // Extract quote without <cite>
-			$cite = substr($custom_message, $cite_start_pos); // Extract everything after, and including, <cite>
+			$cite_start_pos = stripos( $custom_message, '<cite>' );
+			$quote          = substr( $custom_message, 0, $cite_start_pos ); // Extract quote without <cite>
+			$cite           = substr( $custom_message, $cite_start_pos ); // Extract everything after, and including, <cite>
 
 			// Handle quotes that start with <blockquote>
-			if(stripos($custom_message, '<blockquote>') === 0) {
+			if ( stripos( $custom_message, '<blockquote>' ) === 0 ) {
 				$quote = strip_tags( $quote );
 				$quote = '<blockquote>"' . trim( $quote ) . '"';
 			} else {
-				$quote = strip_tags($quote);
-				$quote = '"' . trim($quote) . '"';
+				$quote = strip_tags( $quote );
+				$quote = '"' . trim( $quote ) . '"';
 			}
 
 			// Combine to make updated custom message
